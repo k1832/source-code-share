61f4f1e64d41acf49b92ce53cd8193b53468a9a7
Keita Morisaki <kmns.y.love@icloud.com>
2020-05-14 13:40:50 +0900

    add extract numbers
    1から99までの数字がランダムな順番で連結された文字列から、数字を分解する

---
+++ extract-numbers.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+have_seen = defaultdict(lambda: False)
+element_list = []
+def dfs(string):
+  if not string: return True
+  if string[0] == '0': return False
+  for i in range(1, min(2,len(string))+1):
+    sub_string = string[:i]
+    if have_seen[sub_string]: continue
+    have_seen[sub_string] = True
+    if dfs(string[i:]):
+      element_list.append(sub_string)
+      return True
+    have_seen[sub_string] = False
+  return False
+def main():
+  dfs(string)
+  element_list.reverse()
+  print(element_list)
+if __name__ == "__main__":
+  main()
