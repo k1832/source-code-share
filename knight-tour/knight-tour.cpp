b4c8dfa9162c35dd034c45df4a60bb8a3cfa3f2f
Keita Morisaki <kmns.y.love@icloud.com>
2020-05-26 16:36:16 +0900

    added knight tour cpp version

---
+++ knight-tour/knight-tour.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+const int dy[] = {2,1,-1,-2,-2,-1,1,2}, dx[] = {1,2,2,1,-1,-2,-2,-1};
+int n, board[10][10];
+bool dfs(int y, int x, int current) {
+  if(current == n*n) return true;
+  for(int i = 0; i < 8; ++i) {
+    int next_y = y + dy[i], next_x = x + dx[i];
+    if(next_y < 0 || next_y >= n || next_x < 0 || next_x >= n) continue;
+    if(board[next_y][next_x]) continue;
+    board[next_y][next_x] = current+1;
+    if(dfs(next_y, next_x, current+1)) return true;
+    board[next_y][next_x] = 0;
+  }
+  return false;
+}
+int main() {
+  cin >> n;
+  board[n-1][n-1] = 1;
+  dfs(n-1, n-1, 1);
+  for(int i = 0; i < n; ++i) {
+    for(int j = 0; j < n; ++j) {
+      if(board[i][j] < 10) cout << board[i][j] << "  ";
+      else cout << board[i][j] << " ";
+    }
+    cout << "\n";
+  }
+}
