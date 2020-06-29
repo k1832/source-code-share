2a90e2b112335854e0263fa03d34353c2c9f7300
Keita Morisaki <kmns.y.love@icloud.com>
2020-06-29 14:17:54 +0900

    リファクター

--- shiritori/main.py
+++ shiritori/main.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-import os, copy, string
+import os, copy
 import queue
 base = os.path.join(os.getcwd(), "files")
 
-MAX_NUM_TO_SEARCH = 2
+MAX_NUM_TO_SEARCH = 3
 
 # words_beginning_with = {"a":[WordWithScore], "b": [WordWithScore], ..., "z": [WordWithScore]}
 words_beginning_with = {}
@@ -30,9 +30,7 @@ def update_score():
     for word in words_beginning_with[first_letter]:
       last_letter = word.word[-1].lower()
       word.score = len(words_beginning_with[last_letter])
-  # 最初の文字ごとにscoreでソート
-  for first_letter in words_beginning_with:
-    words_beginning_with[first_letter].sort(key=lambda word: word.score)
+    words_beginning_with[first_letter].sort(key=lambda word: word.score)  # 最初の文字ごとにscoreでソート
   
   # 最後の文字で始まる単語のなかで最もスコアのいいものと自分のスコアを比較
   # 例："abd"について、「"abd"のスコア」と、「"d"で始まる単語の中で最もスコアのいいもの+1」を比較して良い方をscoreとして更新
@@ -42,15 +40,13 @@ def update_score():
       next_best_score = words_beginning_with[last_letter][0].score
       word.score = min([word.score, next_best_score+1])
 
-  # 同じ文字で終わる単語が複数ある場合は、一つに絞る
+  # 同じ文字で終わる単語が複数ある場合は、一つ以外のscoreを極端に大きくする
+  # そうすることでscoreの高い順に探索する時の無駄がなくなる
   for first_letter in words_beginning_with:
     for i in range(len(words_beginning_with[first_letter])-1):
       if words_beginning_with[first_letter][i].word[-1] == words_beginning_with[first_letter][i+1].word[-1]:
         words_beginning_with[first_letter][i+1].score = 100
-  
-  # 最終的なスコアでソート
-  for first_letter in words_beginning_with:
-    words_beginning_with[first_letter].sort(key=lambda word: word.score)
+    words_beginning_with[first_letter].sort(key=lambda word: word.score)  # 最終的なスコアでソート
 
 def bfs(first_letter):
   q = queue.Queue()
